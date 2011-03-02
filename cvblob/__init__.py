130748166e29bae345acd64a95bcd19bad17d3f9
oostendo <nate@oostendorp.net>
2011-03-02 11:53:22 -0500

    add comment header to __init__.py

--- cvblob/__init__.py
+++ cvblob/__init__.py
@@ -1,6 +1,18 @@
+###########################################################################
+#
+#  cvblob/__init__.py
+#
+#  2011 Nathan Oostendorp, Ingenuitas Inc
+#
+#  this is analogous to the cvblob.h file -- giving constants, declaring
+#  the non-native types, and adding a couple helper functions
+#  the real "guts" of this project is in _cvblob.so
+#
+
 import cv;
 from _cvblob import *;
 
+#chaincode constants
 CV_CHAINCODE_UP = 0 # Up.
 CV_CHAINCODE_UP_RIGHT = 1 # Up and right.
 CV_CHAINCODE_RIGHT = 2 # Right.
@@ -10,6 +22,7 @@ CV_CHAINCODE_DOWN_LEFT = 5 # Down and left.
 CV_CHAINCODE_LEFT = 6 # Left.
 CV_CHAINCODE_UP_LEFT = 7 # Up and left.
 
+#blob rendering options
 CV_BLOB_RENDER_COLOR          = 0x0001 # Render each blog with a different color. \see cvRenderBlobs
 CV_BLOB_RENDER_CENTROID       = 0x0002 # Render centroid. \see cvRenderBlobs
 CV_BLOB_RENDER_BOUNDING_BOX   = 0x0004 # Render bounding box. \see cvRenderBlobs
@@ -17,7 +30,7 @@ CV_BLOB_RENDER_ANGLE          = 0x0008 # Render angle. \see cvRenderBlobs
 CV_BLOB_RENDER_TO_LOG         = 0x0010 # Print blob data to log out. \see cvRenderBlobs
 CV_BLOB_RENDER_TO_STD         = 0x0020 # Print blob data to std out. \see cvRenderBlobs
 
-
+#cv track rendering
 CV_TRACK_RENDER_ID           = 0x0001 # Print the ID of each track in the image. \see cvRenderTracks
 CV_TRACK_RENDER_BOUNDING_BOX = 0x0002 # Draw bounding box of each track in the image. \see cvRenderTracks
 CV_TRACK_RENDER_TO_LOG       = 0x0010 # Print track info to log out. \see cvRenderTracks
